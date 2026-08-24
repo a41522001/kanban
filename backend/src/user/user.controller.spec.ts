e2ed3ed09647f1781621d68fd4aec8d2e0c775db
a41522001 <a41522001@gmail.com>
2026-08-24 23:46:00 +0800

    調整user.controller.spec.ts

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -3,14 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { SessionGuard } from '@/session/session.guard';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import type { Request } from 'express';
 describe('UserController', () => {
   let controller: UserController;
 
   const userService = {
     getUserInfo: jest.fn(),
   };
-
+  const createRequest = (userId?: string): Request => {
+    return { userId } as Request;
+  };
   beforeEach(async () => {
     const moduleBuilder = Test.createTestingModule({
       controllers: [UserController],
@@ -36,7 +38,19 @@ describe('UserController', () => {
     controller = module.get(UserController);
   });
 
-  it('應成功建立 Controller', () => {
-    expect(controller).toBeDefined();
+  /** 取得UserInfo */
+  it('取得UserInfo', async () => {
+    const userId = 'testId';
+    const req = createRequest(userId);
+    const res = {
+      email: 'test@email.com',
+      displayName: 'test',
+      avatarUrl: null,
+    };
+    const spy = jest.spyOn(userService, 'getUserInfo').mockResolvedValue(res);
+    const result = await controller.userInfo(req);
+    expect(result).toEqual({ data: res });
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(userId);
   });
 });
